fa6a4c44022049b7b7382fa969de40e88da1795b
damiankadziela81 <damiankadziela02@gmail.com>
2023-04-04 14:11:19 +0200

    Added some descriptions

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -47,3 +47,15 @@ class EmployeeController {
         repository.deleteById(id);
     }
 }
+
+/*
+@RestController indicates that the data returned by each method will be written straight into the response body instead
+of rendering a template.
+
+An EmployeeRepository is injected by constructor into the controller.
+
+We have routes for each operation (@GetMapping, @PostMapping, @PutMapping and @DeleteMapping, corresponding to HTTP GET,
+POST, PUT, and DELETE calls). (NOTE: It’s useful to read each method and understand what they do.)
+
+EmployeeNotFoundException is an exception used to indicate when an employee is looked up but not found.
+ */
--- src/main/java/payroll/EmployeeRepository.java
+++ src/main/java/payroll/EmployeeRepository.java
@@ -4,3 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
+
+/*
+Spring makes accessing data easy. By simply declaring the following EmployeeRepository interface we automatically will
+be able to
+
+Create new Employees
+
+Update existing ones
+
+Delete Employees
+
+Find Employees (one, all, or search by simple or complex properties)
+
+To get all this free functionality, all we had to do was declare an interface which extends Spring Data JPA’s
+JpaRepository, specifying the domain type as Employee and the id type as Long.
+ */
