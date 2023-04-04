cf618cca260b386fee1c3e592aafc9f5d1625fef
damiankadziela81 <damiankadziela02@gmail.com>
2023-04-04 14:10:56 +0200

    Getters and setters changed to public! Added some descriptions

--- src/main/java/payroll/Employee.java
+++ src/main/java/payroll/Employee.java
@@ -21,27 +21,27 @@ class Employee {
         this.role = role;
     }
 
-    Long getId() {
+    public Long getId() {
         return id;
     }
 
-    void setId(final Long id) {
+    public void setId(final Long id) {
         this.id = id;
     }
 
-    String getName() {
+    public String getName() {
         return name;
     }
 
-    void setName(final String name) {
+    public void setName(final String name) {
         this.name = name;
     }
 
-    String getRole() {
+    public String getRole() {
         return role;
     }
 
-    void setRole(final String role) {
+    public void setRole(final String role) {
         this.role = role;
     }
 
@@ -64,3 +64,20 @@ class Employee {
         return "Rmployee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
     }
 }
+
+/*
+Despite being small, this Java class contains much:
+
+@Entity is a JPA annotation to make this object ready for storage in a JPA-based data store.
+
+id, name, and role are attributes of our Employee domain object. id is marked with more JPA annotations to indicate it’s
+the primary key and automatically populated by the JPA provider.
+
+a custom constructor is created when we need to create a new instance, but don’t yet have an id.
+
+With this domain object definition, we can now turn to Spring Data JPA to handle the tedious database interactions.
+
+Spring Data JPA repositories are interfaces with methods supporting creating, reading, updating, and deleting records
+against a back end data store. Some repositories also support data paging, and sorting, where appropriate. Spring Data
+synthesizes implementations based on conventions found in the naming of the methods in the interface.
+ */
