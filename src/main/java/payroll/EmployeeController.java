cdbf68a11f88f95fe45b67cc3ac5cedcc633a49e
damiankadziela81 <damiankadziela02@gmail.com>
2023-04-07 11:19:48 +0200

    Simplified link creation in Controller by adding EmployeeModelAssembler

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -5,7 +5,6 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -15,39 +14,21 @@ class EmployeeController {
 
     private final EmployeeRepository repository;
 
-    EmployeeController(final EmployeeRepository repository) {
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(final EmployeeRepository repository, final EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
-//    List<Employee> all() {
-//        return repository.findAll();
-//    }
     CollectionModel<EntityModel<Employee>> all() {
         List<EntityModel<Employee>> employees = repository.findAll().stream()
-                .map(employee -> EntityModel.of(employee,
-                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
-                .collect(Collectors.toList());
-
+                .map(assembler::toModel)
+                .toList();
         return CollectionModel.of(employees,
                 linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
-    /*
-Wow! That method, which used to just be repository.findAll(), is all grown up! Not to worry. Let’s unpack it.
-
-CollectionModel<> is another Spring HATEOAS container; it’s aimed at encapsulating collections of resources—instead
-of a single resource entity, like EntityModel<> from earlier. CollectionModel<>, too, lets you include links.
-
-Don’t let that first statement slip by. What does "encapsulating collections" mean? Collections of employees?
-
-Not quite.
-
-Since we’re talking REST, it should encapsulate collections of employee resources.
-
-That’s why you fetch all the employees, but then transform them into a list of EntityModel<Employee> objects.
-(Thanks Java 8 Streams!)
-     */
 
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
@@ -55,29 +36,11 @@ That’s why you fetch all the employees, but then transform them into a list of
     }
 
     @GetMapping("/employees/{id}")
-//      Employee one(@PathVariable Long id) {
-//        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
-//    }
     EntityModel<Employee> one(@PathVariable Long id) {
         Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
-
-        return EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
-    /*
-This is very similar to what we had before, but a few things have changed:
-
-The return type of the method has changed from Employee to EntityModel<Employee>. EntityModel<T> is a generic container
-from Spring HATEOAS that includes not only the data but a collection of links.
-
-linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel() asks that Spring HATEOAS build a link to the
-EmployeeController 's one() method, and flag it as a self link.
-
-linkTo(methodOn(EmployeeController.class).all()).withRel("employees") asks Spring HATEOAS to build a link to the
-aggregate root, all(), and call it "employees".
-     */
 
     @PutMapping("/employee/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
