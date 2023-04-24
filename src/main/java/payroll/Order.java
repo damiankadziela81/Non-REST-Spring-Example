010589d32a524892d28578441cf1b32c424ee0fc
damiankadziela81 <damiankadziela02@gmail.com>
2023-04-24 12:31:37 +0200

    Add Order class with conditional links which signal valid state changes to the clients

---
+++ src/main/java/payroll/Order.java
@@ -0,0 +1,76 @@
+package payroll;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "CUSTOMER_ORDER")
+class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String description;
+    private Status status;
+
+    Order() {}
+
+    Order(String description, Status status) {
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus (Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Order order = (Order) o;
+        return id.equals(order.id) && Objects.equals(description, order.description) && status == order.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
+
+/*
+The class requires a JPA @Table annotation changing the table’s name to CUSTOMER_ORDER because ORDER is not a valid
+name for table.
+
+It includes a description field as well as a status field.
+ */
